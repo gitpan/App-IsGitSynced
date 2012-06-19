90d03386055c7c60cf403ad10698d3a6fc7306d1
Иван Бессарабов <ivan@bessarabov.ru>
2012-06-19 22:16:33 +0000

    Import of BESSARABV/App-IsGitSynced-0.03 from CPAN.
    gitpan-cpan-distribution: App-IsGitSynced
    gitpan-cpan-version:      0.03
    gitpan-cpan-path:         BESSARABV/App-IsGitSynced-0.03.tar.gz
    gitpan-cpan-author:       BESSARABV
    gitpan-cpan-maturity:     released

--- lib/App/IsGitSynced.pm
+++ lib/App/IsGitSynced.pm
@@ -8,7 +8,7 @@ App::IsGitSynced - script to find out if the local git repo is fully synced
 
 =head1 VERSION
 
-Version 0.02
+Version 0.03
 
 =head1 SYNOPSIS
 
@@ -18,13 +18,14 @@ is_git_synced [options] dir1 [dir2 ...]
 
       --quiet           Script will not output anything
       --only_errors     Script will write only dirs with errors
+      --show_ok         Show 'ok' message if everthing is synced
       --help            Show this message
       --version         Show version number
 
-Script checks every specifeid dir if it is a git repo and it has ho local
-changes that are not in remote repsitory origin. Script by default will output
-information about every checked dir in separate line. The exit status will
-be 0 if everything is synced and 1 otherwise.
+Script checks every specified dir if it is a git repo and it has ho local
+changes that are not in remote repository origin. Script by default will
+output information about every checking dir in the separate line. The exit
+status will be 0 if everything is synced and 1 otherwise.
 
 Project url: https://github.com/bessarabov/App-IsGitSynced
 
@@ -34,48 +35,13 @@ Ivan Bessarabov, C<< <ivan@bessarabov.ru> >>
 
 =head1 SOURCE CODE
 
-The source code for this module is hosted on GitHub L<https://github.com/bessarabov/App-IsGitSynced>
+The source code for this module is hosted on GitHub
+L<https://github.com/bessarabov/App-IsGitSynced>
 
 =head1 BUGS
 
-Please report any bugs or feature requests to C<bug-app-isgitsynced at rt.cpan.org>, or through
-the web interface at L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=App-IsGitSynced>.  I will be notified, and then you'll
-automatically be notified of progress on your bug as I make changes.
-
-Or you can use GitHub Issues L<https://github.com/bessarabov/App-IsGitSynced/issues>
-
-=head1 SUPPORT
-
-You can find documentation for this module with the perldoc command.
-
-    perldoc App::IsGitSynced
-
-
-You can also look for information at:
-
-=over 4
-
-=item * RT: CPAN's request tracker
-
-L<http://rt.cpan.org/NoAuth/Bugs.html?Dist=App-IsGitSynced>
-
-=item * AnnoCPAN: Annotated CPAN documentation
-
-L<http://annocpan.org/dist/App-IsGitSynced>
-
-=item * CPAN Ratings
-
-L<http://cpanratings.perl.org/d/App-IsGitSynced>
-
-=item * Search CPAN
-
-L<http://search.cpan.org/dist/App-IsGitSynced/>
-
-=back
-
-
-=head1 ACKNOWLEDGEMENTS
-
+Please report any bugs or feature requests in GitHub Issues
+L<https://github.com/bessarabov/App-IsGitSynced/issues>
 
 =head1 LICENSE AND COPYRIGHT
 
@@ -90,6 +56,6 @@ See http://dev.perl.org/licenses/ for more information.
 
 =cut
 
-our $VERSION = 0.02;
+our $VERSION = 0.03;
 
 1;
