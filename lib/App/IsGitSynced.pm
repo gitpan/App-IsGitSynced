44332d61650cc7243f826f4c873404bc1ef6e5bf
Иван Бессарабов <ivan@bessarabov.ru>
2012-07-13 19:55:12 +0000

    Import of BESSARABV/App-IsGitSynced-0.04 from CPAN.
    gitpan-cpan-distribution: App-IsGitSynced
    gitpan-cpan-version:      0.04
    gitpan-cpan-path:         BESSARABV/App-IsGitSynced-0.04.tar.gz
    gitpan-cpan-author:       BESSARABV
    gitpan-cpan-maturity:     released

--- lib/App/IsGitSynced.pm
+++ lib/App/IsGitSynced.pm
@@ -4,11 +4,11 @@ package App::IsGitSynced;
 
 =head1 NAME
 
-App::IsGitSynced - script to find out if the local git repo is fully synced
+App::IsGitSynced - script to find out if the local git repos are fully synced
 
 =head1 VERSION
 
-Version 0.03
+Version 0.04
 
 =head1 SYNOPSIS
 
@@ -18,11 +18,12 @@ is_git_synced [options] dir1 [dir2 ...]
 
       --quiet           Script will not output anything
       --only_errors     Script will write only dirs with errors
+      --ignore_missing  Will ignore missing dirs
       --show_ok         Show 'ok' message if everthing is synced
       --help            Show this message
       --version         Show version number
 
-Script checks every specified dir if it is a git repo and it has ho local
+Script checks every specified dir if it is a git repo and it has no local
 changes that are not in remote repository origin. Script by default will
 output information about every checking dir in the separate line. The exit
 status will be 0 if everything is synced and 1 otherwise.
@@ -53,9 +54,18 @@ by the Free Software Foundation; or the Artistic License.
 
 See http://dev.perl.org/licenses/ for more information.
 
+=begin comment
+
+Subs that check git repo
+
+They return 2 values: 1) $check_status (from %STATUSES) 2) $fail_text that
+should be printed
+
+=end comment
+
 
 =cut
 
-our $VERSION = 0.03;
+our $VERSION = 0.04;
 
 1;
